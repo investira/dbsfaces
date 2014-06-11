1f0b764f8d83f6479645b36bed21cd3b5e79e2d6
ricardo.villar <ricardo.villar@10.37.129.2>
2014-06-11 19:25:23 -0300

    Documentação

--- src/main/resources/META-INF/resources/js/dbsfaces_menu.js
+++ src/main/resources/META-INF/resources/js/dbsfaces_menu.js
@@ -1,23 +1,2 @@
 dbs_menu = function(pId) {
-//Comentado em 9/4/2014 - Resolvido via CSS	
-//	$(".dbs_menu > li").off("mouseover.menu");
-//	$(".dbs_menu > li").on("mouseover.menu", function(e){
-//		$(this).children("ul").addClass("-hover");
-//	});
-//
-//	$(".dbs_menu > li").off("mouseout.menu");
-//	$(".dbs_menu > li").on("mouseout.menu", function(e){
-//		$(this).children("ul").removeClass("-hover");
-//	});
-//	
-//	$(".dbs_menuitem").off("mouseover.menu");
-//	$(".dbs_menuitem").on("mouseover.menu", function(e){
-//		$(this).children(".dbs_submenu").addClass("-hover");
-//	});
-//
-//	$(".dbs_menuitem").off("mouseout.menu");
-//	$(".dbs_menuitem").on("mouseout.menu", function(e){
-//		$(this).children(".dbs_submenu").removeClass("-hover");
-//	});
-
 }
