8c7fe8dcf393dc42ad8286010dd0c09ed2616b3b
ricardo.villar <ricardo.villar@10.37.129.2>
2014-06-13 10:48:52 -0300

    Javascript alterado para controlar a tecla ESC para fechar

--- src/main/resources/META-INF/resources/js/dbsfaces_crudform.js
+++ src/main/resources/META-INF/resources/js/dbsfaces_crudform.js
@@ -4,11 +4,44 @@ dbs_crudForm = function(pId) {
     xH = xH + $(pId + "> .-toolbar").outerHeight();
     
     $(pId).css("padding-bottom", xH + "px");
-    
-    /*
-    
-    $(pId).siblings().find('*').attr("tabindex", "-1");
-    $(pId + " input").prop('disabled', 'disabled');
-	$(pId + " button").prop('disabled', 'disabled');
-	*/
+
+}
+
+dbsfaces.crudForm = {
+	closeEsc: function(e){
+		var tecla_pressionada;
+		if( !e ) {
+			if( window.event ) {
+				//Internet Explorer
+				e = window.event;
+			} else {
+				return;
+			}
+		}
+		if ( typeof( e.keyCode ) == 'number'  ){
+			//DOM
+		   tecla_pressionada = e.keyCode;
+		} else if( typeof( e.which ) == 'number' ){
+		  //NS 4 compatible
+		   tecla_pressionada = e.which;
+		} else if( typeof( e.charCode ) == 'number'  ){
+		  //NS 6+, Mozilla 0.9+
+		   tecla_pressionada = e.charCode;
+		} else {
+		  return;
+		}
+		//Verifica se a tecla ESC foi pressionada
+		if(tecla_pressionada==27){
+			var xCancel = $(".dbs_crudForm [id$='dialog:cancel']").last();
+			if (xCancel.length==0){
+				xCancel = $(".dbs_crudForm [id$='dialog:close']").last();
+			}
+			if (xCancel.length > 0){
+				xCancel.click();
+			}
+			//Recupera a lista de elementos fechar
+//			var xLista = document.getElementsByClassName("fechar");
+//		  	xLista[xLista.length -1].click(); //Clica no último botão da lista, o mais interno.
+		}
+	}
 }
