2c5f858fe30573157acfbc591b519a949ff4b5fd
rvillar <ricardo.villar@dbsoft.com.br>
2014-11-27 19:26:10 -0200

    //Retira os separadores seguidos ou quando for o último item

--- src/main/resources/META-INF/resources/js/dbsfaces_menu.js
+++ src/main/resources/META-INF/resources/js/dbsfaces_menu.js
@@ -1,2 +1,17 @@
 dbs_menu = function(pId) {
+	dbsfaces.menu.eraseOrphans();
+}
+
+dbsfaces.menu = {
+	//Retira os separadores seguidos ou quando for o último item
+	eraseOrphans: function(){
+		$('.dbs_menuitemSeparator').each(function(){
+			var xNext = $(this).next();
+			if(xNext.length == 0){
+				$(this).remove();
+			}else if (xNext.hasClass("dbs_menuitemSeparator")){
+				$(this).remove();
+			}
+		});
+	}
 }
