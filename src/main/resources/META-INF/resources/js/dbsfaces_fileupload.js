087fe37b33bfac2a979d25257106270a452b19f1
rvillar <ricardo.villar@dbsoft.com.br>
2015-09-28 13:59:09 -0300

    1)Aguarda gravação do servidor para enviar mensagem OK 2)Exclusão de console.log 3)Exibição de mensagem com tempo variável

--- src/main/resources/META-INF/resources/js/dbsfaces_fileupload.js
+++ src/main/resources/META-INF/resources/js/dbsfaces_fileupload.js
@@ -1,7 +1,7 @@
 dbs_fileUpload = function(pId, pFileUploadServlet) {
 	var wFile = $(pId).find("input[type='file']");
-	var wBtCancel = $(pId).find("[id*='btCancel']");
-	var wBtStart = $(pId).find("[id*='btStart']");
+	var wBtCancel = $(pId).find("[id$='_btCancel']");
+	var wBtStart = $(pId).find("[id$='_btStart']");
 	var wMessage = $(pId).find(".-message");
 	var wFilesWithError = new Array();
 	var wXHR = 0;
@@ -58,56 +58,39 @@ dbs_fileUpload = function(pId, pFileUploadServlet) {
 	};
 
 	function cancel() {
+//		console.log("UPLOAD CANCEL");
 		//Artificio utlizado para cancelar, pois o abort interrompia todos upload
 		wXHR.open("POST", pFileUploadServlet, true);
 		reset();
+		showMessageError("Upload cancelado!");
 		$(pId).trigger("cancel");
-		console.log("UPLOAD CANCEL");
 
 	};
 	
 	function reset() {
-		console.log("UPLOAD RESET");
+//		console.log("UPLOAD RESET");
 		wBtCancel.children('.-progress').remove();
+		wBtCancel.removeClass("-disabled");
 		wFile.get(0).value = "";
 		wBtStart.show();
 		wBtCancel.hide();
 	};
 	
 	function onloadstartHandler(evt) {
-		console.log("UPLOAD START");
+//		console.log("UPLOAD START");
+		wBtCancel.hide();
 	};
 	
 	function onloadHandler(evt) {
-		var xMsg = "Upload finalizado";
-		if (wFilesWithError.length == 0){
-			xMsg += " com sucesso!";
-		} else{
-			if (wFilesWithError.length == 1){ 
-				xMsg += "! O arquivo " + wFilesWithError[0] + " não foi baixado por exceder";
-			}else{
-				xMsg += "! Os arquivos ";
-				for (i = 0; i < wFilesWithError.length; i++){
-					if (i !=0){
-						xMsg += ",";
-					}
-					xMsg += wFilesWithError[i];
-				}
-				xMsg += " não foram baixados por excederem";
-			}
-			xMsg +=  " o tamanho máximo permitido."; 
-		}
-		showMessage(xMsg);
-		reset();
-		$(pId).trigger("load");
-		console.log("UPLOAD LOAD");
+		wBtCancel.addClass("-disabled");
+//		console.log("UPLOAD LOAD");
 	};
 	
 	function ontimeout(evt) {
+//		console.log("UPLOAD TIMEOUT");
 		showMessageError("Erro de timeout");
 		reset();
 		$(pId).trigger("timeout");
-		console.log("UPLOAD TIMEOUT");
 	};
 
 	function onprogressHandler(evt) {
@@ -116,56 +99,79 @@ dbs_fileUpload = function(pId, pFileUploadServlet) {
 	};
 
 	function onerror(evt) {
+//		console.log("UPLOAD ERROR");
 		showMessageError("Erro na transmissão");
 		reset();
 		$(pId).trigger("error");
-		console.log("UPLOAD ERROR");
 	};
 
 	function onabort(evt) {
+//		console.log("UPLOAD ABORT");
 		showMessageError("Transferencia interrompida");
 		reset();
 		$(pId).trigger("abort");
-		console.log("UPLOAD ABORT");
 	};
 
 	function onreadystatechangeHandler(evt) {
 		var status, text, readyState;
 		try {
 			readyState = evt.target.readyState;
-			text = evt.target.responseText;
+//			text = evt.target.responseText;
 			status = evt.target.status;
 			statusText = evt.target.statusText;
 //			console.log("------------StateChange");
 //			console.log(readyState);
-//			console.log(text);
+////			console.log(text);
 //			console.log(status);
 //			console.log(statusText);
 		}catch(e) {
 			return;
 		}
- 		if (readyState == 4 && status != '200' && statusText) {
- 			showMessageError(statusText);
- 		}
- 		console.log("UPLOAD READY STATE CHANGE");
+		if (readyState == 4){
+			if (status != '200'){
+	 			showMessageError(statusText);
+			}else{
+				statusText = "Upload finalizado";
+				if (wFilesWithError.length == 0){
+					statusText += " com sucesso!";
+				} else{
+					if (wFilesWithError.length == 1){ 
+						statusText += "! O arquivo " + wFilesWithError[0] + " não foi baixado por exceder";
+					}else{
+						statusText += "! Os arquivos ";
+						for (i = 0; i < wFilesWithError.length; i++){
+							if (i !=0){
+								statusText += ",";
+							}
+							statusText += wFilesWithError[i];
+						}
+						statusText += " não foram baixados por excederem";
+					}
+					statusText += " o tamanho máximo permitido."; 
+				}
+	 			showMessage(statusText);
+			}
+			reset();
+			$(pId).trigger("load");
+		}
+// 		console.log("UPLOAD READY STATE CHANGE");
 	};
 
 	function showMessage(pMessage) {
 		wMessage.text(pMessage);
 		wMessage.removeClass("-error");
-		wMessage.fadeIn(200).delay(6000).fadeOut(200);
+		wMessage.fadeIn(200).delay(dbsfaces.ui.getDelayFromTextLength(pMessage)).fadeOut(200);
 	};
 	
 	function showMessageError(pMessage) {
 		wMessage.text(pMessage);
 		wMessage.addClass("-error");
-		wMessage.fadeIn(200).delay(6000).fadeOut(200);
+		wMessage.fadeIn(200).delay(dbsfaces.ui.getDelayFromTextLength(pMessage)).fadeOut(200);
 	};
 
 	function hideMessage() {
 		wMessage.hide();
 	};
-		
-
+	
 };
 
