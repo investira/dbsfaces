5ee6030b09a36e0a5962e163a40c2e3376a316e2
rvillar <ricardo.villar@dbsoft.com.br>
2015-09-28 13:56:24 -0300

    Desabilitar click quando for readOnly

--- src/main/resources/META-INF/resources/js/dbsfaces_button.js
+++ src/main/resources/META-INF/resources/js/dbsfaces_button.js
@@ -1,6 +1,13 @@
 
 dbs_button = function(pId) {
 	dbsfaces.ui.ajaxShowLoading(pId + ".dbs_button");
+	
+	$(pId).on("click", function(e){
+		if ($(this).hasClass("-disabled")){
+			e.stopImmediatePropagation(); 
+			return false;
+		}
+	})
 }
 
 
