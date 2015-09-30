569d467dfadff42738731b6fffee4ad5825ea1d4
rvillar <ricardo.villar@dbsoft.com.br>
2015-09-30 18:31:14 -0300

    Configurar posição somente quando for disparada a exibição pelo timer. Estava gerando problem em mobile

--- src/main/resources/META-INF/resources/js/dbsfaces_tooltip.js
+++ src/main/resources/META-INF/resources/js/dbsfaces_tooltip.js
@@ -1,6 +1,5 @@
 dbs_tooltip = function(pId) {
 	var wTimer = +new Date();
-	
 	$(pId).mouseenter(function(e){
 		dbsfaces.tooltip.showTooltip(pId);
 	});
@@ -27,23 +26,26 @@ dbsfaces.tooltip = {
 		if (xTooltip.css("display") != "none"){
 			return;
 		}
-		var xLeft = $(e).offset().left;
-		var xTop = $(e).offset().top + $(e).outerHeight();
-		/* Se a posição do tooltip for superior ao tamanho do documento, 
-		 * ajusta a posição para exibir acima do componente a que se refere o tooltip
-		 */
-		if (xTop + xTooltip.outerHeight() > $(document).height()){
-			xTop = $(e).offset().top - xTooltip.outerHeight() - 1;
-		}
-		
-		xTooltip.css("top", xTop);
-		xTooltip.css("left", xLeft);
 		
 		//Tempo de exibição
 		var xTime = dbsfaces.ui.getDelayFromTextLength(xTooltip.text());
 		
 		wTimer = setTimeout(function(){
+			var xLeft = $(e).offset().left;
+			var xTop = $(e).offset().top + $(e).outerHeight();
+			/* Se a posição do tooltip for superior ao tamanho do documento, 
+			 * ajusta a posição para exibir próximo do componente a que se refere o tooltip
+			 */
+			if (xTop + xTooltip.outerHeight() > $(document).height()){
+				xTop = $(e).offset().top - xTooltip.outerHeight() - 1;
+			}
+	
+			xTooltip.css("top", xTop);
+			xTooltip.css("left", xLeft);
+
 			xTooltip.show();
+			/* Ajusta para exibir dentro dos limites horizontais da tela
+			 */
 			if (xTooltip.get(0).getBoundingClientRect().left + xTooltip.outerWidth() > $(document).width() ){
 				xLeft = $(document).width() - xTooltip.outerWidth() - 1;
 				xTooltip.css("left", xLeft);
