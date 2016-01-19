f18fed9538a509a54a461d37bb484a6119f64abd
rvillar <ricardo.villar@dbsoft.com.br>
2016-01-19 18:18:19 -0200

    Encode simples com href quando não houver chamada ajax

--- src/main/java/br/com/dbsoft/ui/component/link/DBSLinkRenderer.java
+++ src/main/java/br/com/dbsoft/ui/component/link/DBSLinkRenderer.java
@@ -1,16 +1,20 @@
 package br.com.dbsoft.ui.component.link;
 
 import java.io.IOException;
-
+import java.net.URLEncoder;
 import javax.faces.component.UIComponent;
+import javax.faces.component.UIViewRoot;
 import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
 import javax.faces.event.ActionEvent;
 import javax.faces.render.FacesRenderer;
 
 import com.sun.faces.renderkit.RenderKitUtils;
+import com.sun.faces.renderkit.html_basic.HtmlBasicRenderer.Param;
 
+import br.com.dbsoft.ui.component.DBSPassThruAttributes;
 import br.com.dbsoft.ui.component.DBSRenderer;
+import br.com.dbsoft.ui.component.DBSPassThruAttributes.Key;
 import br.com.dbsoft.ui.core.DBSFaces;
 import br.com.dbsoft.util.DBSString;
 
@@ -46,7 +50,7 @@ public class DBSLinkRenderer extends DBSRenderer {
 		DBSLink xLink = (DBSLink) pComponent;
 		ResponseWriter xWriter = pContext.getResponseWriter();
 		String xClass = DBSFaces.CSS.NOT_SELECTABLE.trim() + " " + xLink.getStyleClass();
-		String xOnClick;
+		String xOnClick = null;
 		String xExecute = "";
 		if (xLink.getExecute() == null){
 			xExecute = getFormId(pContext, pComponent); 
@@ -58,7 +62,12 @@ public class DBSLinkRenderer extends DBSRenderer {
 			xClass += " " + DBSFaces.CSS.MODIFIER.DISABLED;
 		}
 
-		xOnClick = DBSFaces.getSubmitString(xLink, DBSFaces.HTML.EVENTS.ONCLICK, xExecute, xLink.getUpdate());
+		if (xLink.getExecute() != null
+		 || xLink.getUpdate() != null){
+			xOnClick = DBSFaces.getSubmitString(xLink, DBSFaces.HTML.EVENTS.ONCLICK, xExecute, xLink.getUpdate());
+		}else{
+			
+		}
 
 		String xClientId = xLink.getClientId(pContext);
 		if (xLink.getReadOnly()){
@@ -70,12 +79,17 @@ public class DBSLinkRenderer extends DBSRenderer {
 			xWriter.writeAttribute("name", xClientId, "name");
 			xWriter.writeAttribute("class", xClass, "class"); 
 			DBSFaces.setAttribute(xWriter, "style", xLink.getStyle(), null);
-			if (xLink.getActionExpression() != null){				
+			RenderKitUtils.renderPassThruAttributes(pContext, xWriter, xLink, DBSPassThruAttributes.getAttributes(Key.LINK));
+			
+			if (xLink.getActionExpression() != null
+	 		 && xOnClick != null ){				
 //				xWriter.writeAttribute("ontouchstart", "", "ontouchstart"); //Para ipad ativar o css:ACTIVE
 				xWriter.writeAttribute("type", "submit", "type"); 
 				//if (xLink.getUpdate()!=null){
 					DBSFaces.setAttribute(xWriter, DBSFaces.HTML.EVENTS.ONCLICK, xOnClick, null); 
 				//}
+			}else{
+				pvRenderHref(pContext, xWriter, xLink);
 			}
 			xWriter.write(DBSString.toString(xLink.getValue(), ""));
 			DBSFaces.renderChildren(pContext, xLink);
@@ -86,5 +100,56 @@ public class DBSLinkRenderer extends DBSRenderer {
 			xWriter.endElement("a");
 		}
 	}
+	
+	private void pvRenderHref(FacesContext pContext, ResponseWriter pWriter, DBSLink pLink) throws IOException{
+		String hrefVal = pLink.getActionExpression().getExpressionString();
+        // render an empty value for href if it is not specified
+        if (null == hrefVal || 0 == hrefVal.length()) {
+            hrefVal = "";
+        }
 
+        //Write Anchor attributes
+        boolean namespaceParameters = false;
+        Param paramList[] = getParamList(pLink);
+        StringBuffer sb = new StringBuffer();
+        sb.append(hrefVal);
+        boolean paramWritten = (hrefVal.indexOf('?') > 0);
+        String namingContainerId = null;
+        if (namespaceParameters) {
+            UIViewRoot viewRoot = pContext.getViewRoot();
+            namingContainerId = viewRoot.getContainerClientId(pContext);
+        }
+        for (int i = 0, len = paramList.length; i < len; i++) {
+            String pn = paramList[i].name;
+            if (pn != null && pn.length() != 0) {
+            	if (namingContainerId != null) {
+            		pn = namingContainerId + pn;
+            	}
+                String pv = paramList[i].value;
+                sb.append((paramWritten) ? '&' : '?');
+                sb.append(URLEncoder.encode(pn,"UTF-8"));
+                sb.append('=');
+                if (pv != null && pv.length() != 0) {
+                    sb.append(URLEncoder.encode(pv, "UTF-8"));
+                }
+                paramWritten = true;
+            }
+        }
+        sb.append(getFragment(pLink));
+        pWriter.writeURIAttribute("href",
+        			pContext.getExternalContext()
+                                       .encodeResourceURL(sb.toString()),
+                                 "href");
+	}
+
+    protected String getFragment(UIComponent component) {
+
+        String fragment = (String) component.getAttributes().get("fragment");
+        fragment = (fragment != null ? fragment.trim() : "");
+        if (fragment.length() > 0) {
+            fragment = "#" + fragment;
+        }
+        return fragment;
+
+    }
 }
