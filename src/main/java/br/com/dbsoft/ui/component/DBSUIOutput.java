41e2d83f7ff856a7b1d661430cae2c287cf85593
rvillar <ricardo.villar@dbsoft.com.br>
2016-07-15 20:03:39 -0300

    biblioteca para conversão de cores

--- src/main/java/br/com/dbsoft/ui/component/DBSUIOutput.java
+++ src/main/java/br/com/dbsoft/ui/component/DBSUIOutput.java
@@ -52,6 +52,7 @@ import br.com.dbsoft.ui.core.DBSFaces;
 	@ResourceDependency(library = "css", name = "dbsfaces_chartvalue.css", target = "head"),
 	@ResourceDependency(library = "js", name = "jquery-2.1.4.min.js", target = "head"),
 	@ResourceDependency(library = "js", name = "jquery.actual.min.js", target = "head"),
+	@ResourceDependency(library = "js", name = "tinycolor.js", target = "head"),
 	@ResourceDependency(library = "js", name = "dbsmask.js", target = "head"),
 	@ResourceDependency(library = "javax.faces", name = "jsf.js", target = "head"),
 	@ResourceDependency(library = "js", name = "dbsfaces.js", target = "head"),
