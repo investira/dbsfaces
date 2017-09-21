9380edaaa2f71f20a296c7049308480077f2e2d0
rvillar <ricardo.villar@dbsoft.com.br>
2017-09-21 18:59:41 -0300

    Valor do checkbox recuperado em $.val()

--- src/main/resources/META-INF/resources/js/dbsfaces_checkbox.js
+++ src/main/resources/META-INF/resources/js/dbsfaces_checkbox.js
@@ -1,11 +1,26 @@
 dbs_checkbox = function(pId) {
+	dbsfaces.checkbox.setValue($(pId));
 	$(pId).off('click.checkbox')
 		  .on('click.checkbox', function(e){
-		$(e.target).focus();
+			  dbsfaces.checkbox.setValue($(pId));
+			  $(e.target).focus();
 	});
 }
 
 dbsfaces.checkbox = {
+
+	setValue: function(pCheckbox, pValue){
+		var xE = pCheckbox;
+		if (!(xE instanceof jQuery)){
+			xE = $(pCheckbox);
+		}
+		var xInputData = $(dbsfaces.util.jsid(xE[0].id + "-data"));
+		if (typeof pValue != "undefined"){
+			xInputData.val(pValue);
+		}
+		xE.val(xInputData[0].checked);
+	}
+
 }
 
 
