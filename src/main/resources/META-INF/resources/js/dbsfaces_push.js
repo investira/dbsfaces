1bd336a21b2b047733ea953665228715c73b3e96
rvillar <ricardo.villar@dbsoft.com.br>
2017-10-27 16:12:27 -0200

    Melhora no controle da exibição do status

--- src/main/resources/META-INF/resources/js/dbsfaces_push.js
+++ src/main/resources/META-INF/resources/js/dbsfaces_push.js
@@ -9,31 +9,41 @@ dbs_push = function(pId, pUrl, pDelay) {
 	xPush.data("icon" , xPush.children("span"));
 	//Recebe evento update e faz chamada ajax para atualiza-los
 	xEventSource.addEventListener('update', function(e) {
-		var xData = dbsfaces_push.validIds(e.data);
+		var xData = dbsfaces_push.validIds(xPush, e.data);
 		if (xData.length > 0){
-//			jsf.ajax.request($(pId)[0], 'update', {render:xData, execute:'@none', onevent:dbsfaces.onajax, onerror:dbsfaces.onajaxerror});
 			dbsfaces.ajax.request(xPush[0].id, null, xData, dbsfaces_push.status, dbsfaces.ui.showLoadingError(pId), null, pDelay);
 		}
 		return false;
 	}, false);
 
 	xEventSource.addEventListener('error', function(e) {
-		console.log("DBSPush Error\t" + e.data);
+		dbsfaces_push.state(xPush, this.readyState);
+//		console.log("DBSPush Error\t" + e.data);
 	}, false);
 
 	xEventSource.addEventListener('open', function(e) {
+		dbsfaces_push.state(xPush, this.readyState);
 //		console.log("open\t" + e.data);
-		//Não faz nada
 	}, false);
 
 	xEventSource.addEventListener('message', function(e) {
-		dbsfaces_push.showOpen(xPush.data("icon"));
+		dbsfaces_push.state(xPush, this.readyState);
 //		console.log("message\t" + e.data);
 	}, false);
 	
 }
 
 dbsfaces_push = {
+	state: function(pPush, pReadyState){
+		var xDomIcon = pPush.data("icon");
+		if (pReadyState == 0){
+			dbsfaces_push.showConnecting(xDomIcon);
+		}else if (pReadyState == 1){
+			dbsfaces_push.showOpen(xDomIcon);
+		}else if (pReadyState == 2){
+			dbsfaces_push.showClosed(xDomIcon);
+		}
+	},
 	status: function(e){
 		var xDomIcon = $(e.source).data("icon");
 		if (e.status == "success"){
@@ -44,9 +54,15 @@ dbsfaces_push = {
 			console.log(e.status);
 		}
 	},
+	showConnecting: function(pDomIcon){
+		dbsfaces_push.pvShowIcon(pDomIcon, "-i_dot -yellow");
+	},
 	showOpen: function(pDomIcon){
 		dbsfaces_push.pvShowIcon(pDomIcon, "-i_dot -green");
 	},
+	showClosed: function(pDomIcon){
+		dbsfaces_push.pvShowIcon(pDomIcon, "-i_dot -white");
+	},	
 	showUpdate: function(pDomIcon){
 		dbsfaces_push.pvShowIcon(pDomIcon, "-i_dot -blue");
 	},
@@ -64,21 +80,27 @@ dbsfaces_push = {
 	},
 
 	//Retorna somente os componentes que estão momentaneamente com o push suspenso
-	validIds: function(pData){
+	validIds: function(pPush, pData){
 		//Componentes com class -pushDisabled, serão ignorados pelo push 
 		$(".-pushDisabled").each(function(index) {
-			  pData = pDatas.replaceAll($(this).attr("id"),"");
+			  pData = pDatas.replaceAll($(this).attr("id"),"").toLowerCase();
 		});
+		var xPushId = pPush[0].id.toLowerCase();
 		//Cria lista somente com os componentes que existem na tela
 		//para evitar requests desnecessários
+		
 		var xIds = pData.split(/[\s,]+/);
 		var xData = "";
 		for (var i = 0; i < xIds.length; i++) {
-			//Verica se componente existe na tela
-		    if ($(dbsfaces.util.jsid(xIds[i])).length != 0){
-		    	//Adiciona a lista se existir
-		    	xData += xIds[i].replace(/^(:)/,"") + " ";
-		    }
+			var xId = xIds[i];
+			//Ignora o próprio componente push
+			if (xId.toLowerCase() != xPushId){
+				//Verica se componente existe na tela
+			    if ($(dbsfaces.util.jsid(xId)).length != 0){
+			    	//Adiciona a lista se existir
+			    	xData += xIds[i].replace(/^(:)/,"") + " ";
+			    }
+			}
 		}
 		return xData.trim();
 	}									
