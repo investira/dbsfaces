881d2ba13794741c56e33c36f2d5807d1724b711
rvillar <ricardo.villar@dbsoft.com.br>
2018-03-20 17:47:28 -0300

    Novos métodos moveIn, moveOutLeft e moveOutRight

--- src/main/resources/META-INF/resources/js/dbsfaces_t.js
+++ src/main/resources/META-INF/resources/js/dbsfaces_t.js
@@ -1,11 +1,19 @@
 $(document).ready(function(){
 	//SLIDER
 	$(".-t_slide > div").on(dbsfaces.EVENT.ON_TRANSITION_END, function(e){
-		var xThis = $(this)
+		var xThis = $(this);
+		var xParent = xThis.parent();
 		var xNext = xThis.next();
 		if (xNext.length > 0){
-			xNext.css("left", $(this).css("left"));
-			dbsfaces.ui.cssAllBrowser(xNext, "transition-timing-function",  xThis.css("transition-timing-function"));
+			if (xParent.hasClass("-in")){
+				dbsfaces.t.slide.moveIn(xNext);
+			}else if (xParent.hasClass("-out_left")){
+				dbsfaces.t.slide.moveOutLeft(xNext);
+			}else if (xParent.hasClass("-out_right")){
+				dbsfaces.t.slide.moveOutRight(xNext);
+			}
+//			xNext.css("left", $(this).css("left"));
+//			dbsfaces.ui.cssAllBrowser(xNext, "transition-timing-function",  xThis.css("transition-timing-function"));
 		}
 		return false;
 	});
@@ -15,22 +23,36 @@ $(document).ready(function(){
 dbsfaces.t = {}
 
 dbsfaces.t.slide = {
-	slideIn: function(pParent){
-		var xItem = pParent.children().first();
-		xItem.css("left","0");
-		dbsfaces.ui.cssAllBrowser(xItem, "transition-timing-function", "ease-out");
-	},
+	moveIn: function(pSlide){
+		dbsfaces.t.slide.move(pSlide, "-in");
+	},	
+	moveOutLeft: function(pSlide){
+		dbsfaces.t.slide.move(pSlide, "-out_left");
+	},	
+	moveOutRight: function(pSlide){
+		dbsfaces.t.slide.move(pSlide, "-out_right");
+	},	
+	move: function(pSlide, pPosition){
+		pSlide.removeClass("-out_left").removeClass("-out_right").removeClass("-in").addClass(pPosition);
+	}	
 
-	slideLeft: function(pParent){
-		var xItem = pParent.children().first();
-		xItem.css("left","-100vw");
-		dbsfaces.ui.cssAllBrowser(xItem, "transition-timing-function", "ease-in");
-	},
-		
-	slideRight: function(pParent){
-		var xItem = pParent.children().first();
-		xItem.css("left","100vw");
-		dbsfaces.ui.cssAllBrowser(xItem, "transition-timing-function", "ease-in");
-	}
 }
+//	slideIn: function(pParent){
+//		var xItem = pParent.children().first();
+//		xItem.css("left","0");
+//		dbsfaces.ui.cssAllBrowser(xItem, "transition-timing-function", "ease-out");
+//	},
+//
+//	slideLeft: function(pParent){
+//		var xItem = pParent.children().first();
+//		xItem.css("left","-100vw");
+//		dbsfaces.ui.cssAllBrowser(xItem, "transition-timing-function", "ease-in");
+//	},
+//		
+//	slideRight: function(pParent){
+//		var xItem = pParent.children().first();
+//		xItem.css("left","100vw");
+//		dbsfaces.ui.cssAllBrowser(xItem, "transition-timing-function", "ease-in");
+//	}
+//}
 
